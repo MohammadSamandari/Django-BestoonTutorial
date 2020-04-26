3bf347120f96a44f98836151e566a229b1ee7318
lord <samandari.mohammad@gmail.com>
2020-04-27 00:00:19 +0430

    removing login pages and showing the data in the index page && deploying the program so that static files are created.

--- bestoon/wsgi.py
+++ bestoon/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+import sys
+path= '/home/lord/django_project/bestoon'
+if path not in sys.path:
+    sys.path.append(path)
+
+
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bestoon.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
